f3c8fd5f8aeab5c72251d9db2104a5dcbe1f0fa0
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2021-07-10 08:31:15 +0900

    Remove [AllowAnonymous] from Logout. With this, developers are sure that an authorized user is calling Logout, and developers can extract information of that user (without checking authorization condition with an IF-ELSE). Therefore it is easier to handle things before really log the user out.
    QUESTION: What if an anonymous call Logout?
    
    ANSWER: "HandleUnauthorizedRequest" (WebApplication1.Attributes.CustomAuthorizeAttribute.HandleUnauthorizedRequest) will be called, and they will be redirected to "Login/Index".

--- Code/WebApplication1/Controllers/LoginController.cs
+++ Code/WebApplication1/Controllers/LoginController.cs
@@ -36,7 +36,6 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        [AllowAnonymous]
         public ActionResult Logout()
         {
             HttpContext.ClearAuthorizationCookie();
