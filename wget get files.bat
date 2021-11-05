f68d8683b1d33eaf14564a0fcf8186a9e7ea8b96
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2021-11-05 11:37:24 +0900

    A demo of Opentip: apply tooltip on Login button.
    I use wget to get libraries from a third-party repository.

---
+++ wget get files.bat
@@ -0,0 +1,4 @@
+wget -P Code/WebApplication1/Scripts/opentip/lib https://raw.githubusercontent.com/enyo/opentip/master/lib/opentip.js
+wget -P Code/WebApplication1/Scripts/opentip/lib https://raw.githubusercontent.com/enyo/opentip/master/lib/adapter-native.js
+wget -P Code/WebApplication1/Scripts/opentip/lib https://raw.githubusercontent.com/enyo/opentip/master/lib/adapter-jquery.js
+wget -P Code/WebApplication1/Content/opentip/css https://raw.githubusercontent.com/enyo/opentip/master/css/opentip.css
