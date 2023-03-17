6d2ae790ec5f3e96d8e18ee326df9099f0b6f21b
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2023-03-17 13:02:32 +0900

    Use toggle to show/hide password. Can toggle with keyboard.

--- Code/WebApplication1/Scripts/LoginController_Index.js
+++ Code/WebApplication1/Scripts/LoginController_Index.js
@@ -2,45 +2,16 @@
 var tip1 = '<span style="color:#007bff;">Login button</br><b>Opentip example</b></span>';
 new Opentip('#login-btn', tip1, { tipJoint: 'top', background: '#ff8400', borderColor: '#000000' });
 
-// hold/release to show/hide password - start
+// toggle to show/hide password - start
 exampleInputPassword1 = document.getElementById("exampleInputPassword1");
-function hold(target) {
-    exampleInputPassword1.type = "text";
-}
-function release(target) {
-    exampleInputPassword1.type = "password";
-}
-
+hiddenCheckbox = document.getElementById("toggle-hidden-checkbox");
 trigger = document.getElementById("toggle-trigger");
-trigger.addEventListener('mousedown', event => {
-    hold(event.currentTarget);
-});
-trigger.addEventListener('touchstart', event => {
-    hold(event.currentTarget);
-});
-trigger.addEventListener('mouseup', event => {
-    release(event.currentTarget);
-});
-trigger.addEventListener('mouseleave', event => {
-    release(event.currentTarget);
-});
-trigger.addEventListener('mouseout', event => {
-    release(event.currentTarget);
-});
-trigger.addEventListener('touchend', event => {
-    release(event.currentTarget);
-});
-trigger.addEventListener('touchcancel', event => {
-    release(event.currentTarget);
-});
-trigger.addEventListener('keydown', event => {
-    if (event.code === 'Space') {
-        hold(event.currentTarget);
+trigger.addEventListener('click', event => {
+    hiddenCheckbox.checked = !hiddenCheckbox.checked;
+    if (hiddenCheckbox.checked) {
+        exampleInputPassword1.type = "text";
+    } else {
+        exampleInputPassword1.type = "password";
     }
 });
-trigger.addEventListener('keyup', event => {
-    if (event.code === 'Space') {
-        release(event.currentTarget);
-    }
-});
-// hold/release to show/hide password - end
+// toggle to show/hide password - end
