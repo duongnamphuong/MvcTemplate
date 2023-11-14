5b84a30fa44aa0657589f6e9862ffe296f4a4309
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2023-11-14 10:12:43 +0900

    update batch: "wget get files.bat": add -N option (Will only download a file if it’s newer than a file of the same name on your computer.)

--- wget get files.bat
+++ wget get files.bat
@@ -1,5 +1,5 @@
-wget -P Code/WebApplication1/Scripts/highcharts https://code.highcharts.com/highcharts.src.js
-wget -P Code/WebApplication1/Scripts/opentip/lib https://raw.githubusercontent.com/enyo/opentip/master/lib/opentip.js
-wget -P Code/WebApplication1/Scripts/opentip/lib https://raw.githubusercontent.com/enyo/opentip/master/lib/adapter-native.js
-wget -P Code/WebApplication1/Scripts/opentip/lib https://raw.githubusercontent.com/enyo/opentip/master/lib/adapter-jquery.js
-wget -P Code/WebApplication1/Content/opentip/css https://raw.githubusercontent.com/enyo/opentip/master/css/opentip.css
+wget -N -P Code/WebApplication1/Scripts/highcharts https://code.highcharts.com/highcharts.src.js
+wget -N -P Code/WebApplication1/Scripts/opentip/lib https://raw.githubusercontent.com/enyo/opentip/master/lib/opentip.js
+wget -N -P Code/WebApplication1/Scripts/opentip/lib https://raw.githubusercontent.com/enyo/opentip/master/lib/adapter-native.js
+wget -N -P Code/WebApplication1/Scripts/opentip/lib https://raw.githubusercontent.com/enyo/opentip/master/lib/adapter-jquery.js
+wget -N -P Code/WebApplication1/Content/opentip/css https://raw.githubusercontent.com/enyo/opentip/master/css/opentip.css
